32d00a94589c95eccfd0ec6612299a3969bad197
Evan Katsuranis <e.katsuranis@westernhealth.com>
2017-02-16 14:29:44 -0800

    refactor plan template into seperate mod
    need to show how to use modules in node

---
+++ plan-app/PlanPage.js
@@ -0,0 +1,17 @@
+exports.render  = function( plans ) {
+  return `<!doctype html>
+
+    <head>
+      <title>Plan App</title>
+    </head>
+
+    <body>
+      <h1>Hello, World</h1>
+      <ul>${ plans.map( renderPlan ).join("") }</ul>
+    </body>
+  `
+}
+
+var renderPlan = function( plan ) {
+  return `<li>${ plan.planName }</li>`
+}
--- plan-app/index.js
+++ plan-app/index.js
@@ -1,30 +1,14 @@
 var Express = require( "express" )
 var Request = require('request');
 
+var PlanPage = require( "./PlanPage" )
+
 var app = Express()
-
-var renderPage = function( plans ) {
-  return `<!doctype html>
-
-    <head>
-      <title>Plan App</title>
-    </head>
-
-    <body>
-      <h1>Hello, World</h1>
-      <ul>${ plans.map( renderPlan ).join("") }</ul>
-    </body>
-  `
-}
-
-var renderPlan = function( plan ) {
-  return `<li>${ plan.planName }</li>`
-}
 
 app.get( "/", function( req, res ) {
   Request.get('http://localhost:8000', function(err, planServiceResponse, body) {
-    res.end(renderPage(JSON.parse(body)))
-  });
+    res.end(PlanPage.render(JSON.parse(body)) )
+  } );
 } )
 
 app.listen(8001);
