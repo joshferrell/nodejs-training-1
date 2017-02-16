b051003a031d0b002a30ddfc6bb0e45d98671e7c
Evan Katsuranis <e.katsuranis@westernhealth.com>
2017-02-16 13:39:42 -0800

    fix issue with atom added newline in csv file

--- plan-service/index.tests.js
+++ plan-service/index.tests.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 request.get('http://localhost:8000', function(err, res, body) {
     assert.equal(res.statusCode, 200);
     assert.deepEqual(JSON.parse(body), [
-      {"planName":"Great HMO","planBenefits":" benefits package 1","group":" UCD"},
-      {"planName":"Silver PPO","planBenefits":" benefits package 2","group":" WHA"}
+      {"planName":"Great HMO","planBenefits":"benefits package 1","group":"UCD"},
+      {"planName":"Silver PPO","planBenefits":"benefits package 2","group":"WHA"}
     ]);
 });
