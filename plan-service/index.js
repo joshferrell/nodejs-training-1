351f0596a4ff6505a8580f76b0b7720a130f7fda
Joshua Ferrell <jferrell@wha-mbp5.local>
2017-02-24 10:47:25 -0800

    chore: added linting to application and passed linting mod

--- plan-service/index.js
+++ plan-service/index.js
@@ -1,22 +1,18 @@
-var http = require( "http" )
-var fs = require( "fs" )
+var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function(req, res) {
 
-  fs.readFile( "plans.csv", "utf8", function(err, data) {
-    if (err) {
-      res.send(JSON.stringify(err))
-    }
+    fs.readFile("plans.csv", "utf8", function(err, data) {
+        if (err) {
+            res.send(JSON.stringify(err));
+        }
 
-    var plans = data
-      .split("\n")
-      .map(l => l.split(","))
-      .filter(l => l[0] != "" )
-      .map(l => ({ planName: l[0], planBenefits: l[1], group: l[2], memberId: l[3] }) )
+        var plans = data.split("\n").map(l => l.split(",")).filter(l => l[0] != "").map(l => ({planName: l[0], planBenefits: l[1], group: l[2], memberId: l[3]}));
 
-    res.end(JSON.stringify(plans))
-  })
+        res.end(JSON.stringify(plans));
+    })
 
 })
 
-server.listen(8000)
+server.listen(8000);
