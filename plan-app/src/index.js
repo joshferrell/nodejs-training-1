17c5d234c40377c86514393215b68131ef983ce0
Erick Fleming <efleming969@gmail.com>
2017-02-24 07:20:05 -0800

    commented out database login and plans

--- plan-app/src/index.js
+++ plan-app/src/index.js
@@ -2,101 +2,125 @@ var Request = require( "request" )
 var Express = require( "express" )
 var Session = require( "express-session" )
 var BodyParser = require( "body-parser" )
-var Sqlite = require( "sqlite3" ).verbose()
 
-var IndexPage = require( "./IndexPage" )
-var LoginPage = require( "./LoginPage" )
-
+// initializing express application
 var app = Express()
-var db = new Sqlite.Database( "members.db" )
 
-var assignDefaultPlan = function( defaultName ) {
-  return function( plan ) {
-    return Object.assign( plan
-    , { checked: plan.planName === defaultName ? "checked" : "" } )
-  }
-}
-
-
-app.set( "views", __dirname + "/views" )
-
+// setting up express middleware
 app.use( Session( { secret: "supersecret" } ) )
 app.use( BodyParser.urlencoded( { extended: true } ) )
 
-app.get( "/login", function( req, res ) {
-  res.end( LoginPage.render() )
-} )
-
-app.post( "/logout", function( req, res ) {
-  req.session.user = undefined
-  res.redirect( "/" )
-} )
-
-app.post( "/login", function( req, res ) {
-  var sql = "SELECT * FROM members WHERE id = ?"
-
-  db.serialize(function() {
-    db.all( sql, [ req.body.id ], function(err, rows) {
-      if(rows.length > 0) {
-        req.session.user = { id: req.body.id }
-        res.redirect( "/" )
-      }
-      else {
-        res.end(`
-          <h1>Error</h1>
-          <p>I don't know who you are, click <a href="/">here</a> to start again!</p>
-        `)
-      }
-    } )
-  } )
-} )
-
+// setting up root router
 app.get( "/", function( req, res ) {
   if ( req.session.user === undefined ) {
     res.redirect( "/login" )
   }
   else {
-    Request.get('http://localhost:8000', function(err, planServiceResponse, plans ) {
-      var sql = "SELECT * FROM members WHERE id = ?"
-      var memberId = req.session.user.id
-
-      db.get( sql, [ memberId ], function(err, row) {
-        if(row == undefined ) {
-          res.end("<h1>I don't know who you are</h1>")
-        }
-        else {
-          var viewModel = {
-            plans: JSON
-              .parse( plans )
-              .map( assignDefaultPlan( row.default_plan ) )
-          }
-
-          res.render( "index.hbs", viewModel )
-        }
-      } )
-    } )
+    res.send( `<h1>Welcome ${ req.session.user.name }</h1>` )
   }
 } )
 
-app.post( "/plans", function( req, res ) {
-  if ( req.session.user == undefined ) {
-    res.redirect( "/login" )
+// setting up login page router
+app.get( "/login", function( req, res ) {
+  res.send( `
+    <!doctype html>
+
+    <head>
+      <title>Plan App</title>
+    </head>
+
+    <body>
+      <h1>Login</h1>
+      <p>Please enter your id to login</p>
+      <form action="/login" method="POST">
+        Id: <input type="text" name="id"><button>Submit</button>
+      </form>
+    </body>
+  ` )
+} )
+
+// setup route for logging in
+app.post( "/login", function( req, res ) {
+  var memberId = req.body.id
+
+  if( memberId && memberId == "1" ) {
+    req.session.user = { id: "1", name: "john" }
+    res.redirect( "/" )
   }
   else {
-    var sql = "UPDATE members SET default_plan = ? WHERE id = ?"
-    var params = [ req.body.defaultPlan, req.session.user.id ]
-
-    db.run( sql, params, function( err ) {
-      res.redirect( "/" )
-    } )
+    res.send(`
+      <h1>Error</h1>
+      <p>I don't know who you are, click <a href="/">here</a> to start again!</p>
+    `)
   }
 } )
 
+// starting express application
 app.listen(8001)
 
-db.serialize( function() {
-  db.run("DROP TABLE IF EXISTS members" )
-  db.run("CREATE TABLE members(id varchar(255), name varchar(255), default_plan varchar(255))")
-  db.run("INSERT INTO members(id, name, default_plan) VALUES('1', 'john', '')")
-  console.log( "databse was setup!" )
-} )
+// db.serialize( function() {
+//   db.run("DROP TABLE IF EXISTS members" )
+//   db.run("CREATE TABLE members(id varchar(255), name varchar(255), default_plan varchar(255))")
+//   db.run("INSERT INTO members(id, name, default_plan) VALUES('1', 'john', '')")
+//   console.log( "databse was setup!" )
+// } )
+
+// var assignDefaultPlan = function( defaultName ) {
+//   return function( plan ) {
+//     return Object.assign( plan
+//     , { checked: plan.planName === defaultName ? "checked" : "" } )
+//   }
+// }
+
+// app.get( "/", function( req, res ) {
+//   if ( req.session.user === undefined ) {
+//     res.redirect( "/login" )
+//   }
+//   else {
+//     Request.get('http://localhost:8000', function(err, planServiceResponse, plans ) {
+//       var sql = "SELECT * FROM members WHERE id = ?"
+//       var memberId = req.session.user.id
+//
+//       db.get( sql, [ memberId ], function(err, row) {
+//         if(row == undefined ) {
+//           res.end("<h1>I don't know who you are</h1>")
+//         }
+//         else {
+//           var viewModel = {
+//             plans: JSON
+//               .parse( plans )
+//               .map( assignDefaultPlan( row.default_plan ) )
+//           }
+//
+//           res.render( "index.hbs", viewModel )
+//         }
+//       } )
+//     } )
+//   }
+// } )
+//
+// app.post( "/plans", function( req, res ) {
+//   if ( req.session.user == undefined ) {
+//     res.redirect( "/login" )
+//   }
+//   else {
+//     var sql = "UPDATE members SET default_plan = ? WHERE id = ?"
+//     var params = [ req.body.defaultPlan, req.session.user.id ]
+//
+//     db.run( sql, params, function( err ) {
+//       res.redirect( "/" )
+//     } )
+//   }
+// } )
+//
+// app.post( "/logout", function( req, res ) {
+//   req.session.user = undefined
+//   res.redirect( "/" )
+// } )
+//
+// var IndexPage = require( "./IndexPage" )
+// var LoginPage = require( "./LoginPage" )
+
+// app.set( "views", __dirname + "/views" )
+// var Sqlite = require( "sqlite3" ).verbose()
+// var db = new Sqlite.Database( "members.db" )
